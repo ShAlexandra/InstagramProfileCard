15102fcb818a35901a97d30e20e78dd3ce912318
Alexandra <shalexandra2000@gmail.com>
2023-12-30 15:09:36 +0300

    created a Card

--- app/src/main/java/com/example/firstcompose/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/example/firstcompose/ui/theme/InstagramProfileCard.kt
@@ -1,5 +1,6 @@
 package com.example.firstcompose.ui.theme
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,6 +10,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,22 +23,38 @@ import androidx.compose.ui.unit.dp
 @Preview
 @Composable
 fun InstagramProfileCard() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Box(
-            modifier = Modifier
-                .background(color = Color.Yellow)
-                .size(50.dp)
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+        ),
+//        modifier = Modifier
+//            .background(Color.White),
+        shape = RoundedCornerShape(
+            topStart = 4.dp,
+            topEnd = 4.dp
+        ),
+        border = BorderStroke(
+            width = 1.dp,
+            color = Color.Black
         )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(color = Color.Yellow)
+                    .size(50.dp)
+            )
 
-        TwoBoxes()
-        TwoBoxes()
-        TwoBoxes()
+            TwoBoxes()
+            TwoBoxes()
+            TwoBoxes()
+        }
     }
 }
 
