a121f659630648028f3ad90b7af457daef38d7d7
Alexandra <shalexandra2000@gmail.com>
2023-12-30 14:10:17 +0300

    initial commit

---
+++ app/src/main/java/com/example/firstcompose/ui/theme/InstagramProfileCard.kt
@@ -0,0 +1,11 @@
+package com.example.firstcompose.ui.theme
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+
+@Composable
+fun InstagramProfileCard() {
+    Row {
+
+    }
+}
