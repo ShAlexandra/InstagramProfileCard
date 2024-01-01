3a3430c9fcadae830e11c6fd923776883e545a4c
Alexandra <shalexandra2000@gmail.com>
2024-01-01 18:25:39 +0300

    idd instagram logo

--- app/src/main/java/com/example/firstcompose/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/example/firstcompose/ui/theme/InstagramProfileCard.kt
@@ -1,15 +1,16 @@
 package com.example.firstcompose.ui.theme
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -18,12 +19,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.firstcompose.R
 
 @Composable
 fun InstagramProfileCard() {
@@ -49,16 +53,21 @@ fun InstagramProfileCard() {
             horizontalArrangement = Arrangement.SpaceEvenly,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Box(
+            Image(
+                painter = painterResource(id = R.drawable.ic_instagram),
+                contentDescription = null,
                 modifier = Modifier
-                    .background(color = Color.Yellow)
-                    .size(50.dp)
+                    .clip(CircleShape)
+                    .size(60.dp)
+                    .background(Color.White)
+                    .padding(8.dp)
             )
 
             UserStatistics(title = "Posts", value = "6,950")
             UserStatistics(title = "Followers", value = "436M")
             UserStatistics(title = "Following", value = "76")
         }
+
     }
 }
 
